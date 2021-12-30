492d14100d671bf0a58d3eec230b5d9d4bcbfea1
Matthew Cross <matthewcross1017@gmail.com>
2021-12-29 19:28:13 -0500

    Restart Database

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -1,9 +1,10 @@
+
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
+  const name = document.querySelector('#post-name').value.trim();
   const postTopic = document.querySelector('#post-topic').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const description = document.querySelector('#post-desc').value.trim();
 
   if (name && postTopic && description) {
     const response = await fetch(`/api/post`, {
@@ -17,15 +18,15 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create post');
     }
   }
 };
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
+   
     const id = event.target.getAttribute('data-id');
-
     const response = await fetch(`/api/post/${id}`, {
       method: 'DELETE',
     });
@@ -33,15 +34,34 @@ const delButtonHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete post');
     }
   }
 };
 
+// const updateHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+   
+//     const id = event.target.getAttribute('data-id');
+//     const response = await fetch(`/api/post/${id}`, {
+//       method: 'PATCH',
+//     });
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert('Failed to update post');
+//     }
+//   }
+// };
+
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.delete')
   .addEventListener('click', delButtonHandler);
+
+// document 
+// .querySelector('.update')
+// .addEventListener('click', updateHandler);
