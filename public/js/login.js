a243bbf5169eb0767d4b6d2c9272e8d68d629a26
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-14 16:38:38 -0500

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      console.log(Successful Sign-up!!)
+      
       document.location.replace('/dashboard');
     } else {
       response.json()
@@ -40,7 +40,9 @@ const signupFormHandler = async (event) => {
       headers: { 'Content-Type': 'application/json' },
     });
     if (response.ok) {
+       console.log(Successful Sign-up!!),
       document.location.replace('/dashboard');
+     
     } else {
       response.json()
       .then(data => {
